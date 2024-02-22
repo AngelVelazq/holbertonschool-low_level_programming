b02ed48685e698872862de8f94ebca1396cff6ca
AngelVelazq <angelvelazquez.op@gmail.com>
2024-02-22 12:34:12 -0400

    print lowercase n !=q&&e

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,8 +9,12 @@ int main(void)
 {
 	char i;
 
-	for (i = 'a' ; i <= 'z')
-		if (n != 'q' && n != 'e')
-			putchar (i);
+	for (i = 'a' ; i <= 'z'; i++)
+	{
+	if (i != 'q' && i != 'e')
+	putchar(i);
+	}
 	putchar ('\n');
+
+	return (0);
 }
