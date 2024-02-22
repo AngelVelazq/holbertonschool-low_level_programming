ed7632b271383ba5656505e8146eadab82aa50d6
AngelVelazq <angelvelazquez.op@gmail.com>
2024-02-22 11:57:25 -0400

    print lowercase except q&e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z')
+		if (n != 'q' && n != 'e')
+			putchar (i);
+	putchar ('\n');
+}
