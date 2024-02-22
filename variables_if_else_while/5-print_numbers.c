754b02bd4c4b68500b6845fda4711b748f3c0ed0
AngelVelazq <angelvelazquez.op@gmail.com>
2024-02-22 13:58:01 -0400

    print 0-9 of base 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x <= 10 ; x++)
+	{
+	if (x != 10)
+	putchar(x);
+	}
+	putchar ('\n');
+	return (0);
+}
