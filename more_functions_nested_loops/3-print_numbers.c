ad8239561e495d5c50da57904c608b0540b4ec17
AngelVelazq <angelvelazquez.op@gmail.com>
2024-02-26 13:48:35 -0400

    print from 0-9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers - Print numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	char x;
+
+	for (x = '0'; x <= '9'; x++)
+	_putchar(x);
+	_putchar('\n');
+}
