9ff1f24060be3c8a3ebdf26f7e411048a652cc58
AngelVelazq <angelvelazquez.op@gmail.com>
2024-02-27 11:30:26 -0400

    draws a straight line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_line - Write a function that draws a straight line
+ * @n: Number of characters to print in the line
+ */
+void print_line(int n)
+{
+	int l;
+
+	if (n > 0)
+	{
+		for (l = 0; l <= n; l++)
+		{
+			_putchar('_');
+		}
+		if (n <= 0)
+		{
+			_putchar('\n');	
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
