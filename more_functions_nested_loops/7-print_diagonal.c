2f195eeeb25e067da0abbe268ed9e8d8bbbb8c27
AngelVelazq <angelvelazquez.op@gmail.com>
2024-02-28 10:02:54 -0400

    print diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_diagonal - Write a function that draws a diagonal line 
+ * print_diagonal - Write a function that draws a diagonal line
  * @n: number of characters to be printed
  */
 void print_diagonal(int n)
 {
-        int space, rows;
+	int space, rows;
 
-        if (n > 0)
-        {
-                for (rows = 1; rows <= n; rows++)
+	if (n > 0)
+	{
+		for (rows = 1; rows <= n; rows++)
 		{
 			for (space = 0; space < rows; space++)
 			{
