982dccb06072f127a48631c0a90445620592db0d
AngelVelazq <angelvelazquez.op@gmail.com>
2024-03-01 13:14:55 -0400

    points n value to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - Takes a  pointer to an int and updates the value points to 98
+ * @n: An integer input pointer
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
