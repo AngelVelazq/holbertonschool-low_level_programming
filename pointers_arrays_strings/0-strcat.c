888a5bd9e5dfd535088bf9c86fd9ca49d3e5cec5
AngelVelazq <angelvelazquez.op@gmail.com>
2024-03-02 17:26:23 -0400

    concatenates two strings

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * _strcat - concatenates two strings using at most
+ * *_strcat - concatenates two strings using at most
  * an inputted number of bytes from src
  * @dest: the string to be appended upon
  * @src: the string to be appended to dest
  * @n: the number of bytes from src to be appended to dest
  * Return: a pointer to the resulting string dest
  */
-char *_strcat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int index = 0, dest_len = 0, n = 0;
+
 
 	while (dest[index++])
 		dest_len++;
