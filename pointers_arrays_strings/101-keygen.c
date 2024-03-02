a5553fe6f2ee52ab9d33c838f18714a9260d92fd
AngelVelazq <angelvelazquez.op@gmail.com>
2024-03-02 17:17:40 -0400

    Concatenates two strings

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -8,28 +8,30 @@
 int main(void)
 {
 	char password[84];
-	int index = 0, sum = 0, diff_half1, diff_half2;
+	int i = 0, sum = 0, diff_half1 = 0, diff_half2 = 0;
 
-	srand(time(0));
+	srand((unsigned int)time(0));
 
 	while (sum < 2772)
 	{
-		password[index] = 33 + rand() % 94;
-		sum += password[index++];
+		password[i] = 33 + rand() % 94;
+		sum += password[i++];
 	}
-	password[index] = '\0';
+	password[i] = '\0';
 	if (sum != 2772)
 	{
 		diff_half1 = (sum - 2772) / 2;
 		diff_half2 = (sum - 2772) / 2;
 
 		if ((sum - 2772) % 2 != 0)
-			diff_half1++;
-		for (index = 0; password[index]; index++)
 		{
-			if (password[index] >= (33 + diff_half1))
+			diff_half1++;
+		}
+		for (i = 0; password[i]; i++)
+		{
+			if (password[i] >= (33 + diff_half2))
 			{
-				password[index] -= diff_half1;
+				password[i] -= diff_half2;
 				break;
 			}
 		}
