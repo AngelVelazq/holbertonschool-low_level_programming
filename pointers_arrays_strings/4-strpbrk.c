e5a8cc8a30ed6c312fee49f42a736c63f446fa80
AngelVelazq <angelvelazquez.op@gmail.com>
2024-03-07 10:30:20 -0400

    search string for set of bytes

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strpbrk - function to locate string
+ * @s: first string
+ * @accept: pointer to match
+ * Return: null
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	const char *p;
+
+	while (*s)
+	{
+		if (*s == *p)
+		{
+			return (s);
+			p++;
+		}
+		s++;
+	}
+	return ('\0');
+}
