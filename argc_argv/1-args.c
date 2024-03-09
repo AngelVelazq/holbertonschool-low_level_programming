08e01814d165c53301899eeeaef54988f4900cf6
AngelVelazq <angelvelazquez.op@gmail.com>
2024-03-09 19:36:33 -0400

    prints the number of arguments passed

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -3,11 +3,11 @@
  * main - Entry point
  * @argc: number of arguments passed
  * @argv: number of strings
- * Return: void
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
 	(void) argv;
-	printf("argc = %d\n", argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
