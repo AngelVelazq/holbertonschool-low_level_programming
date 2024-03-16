7646e30426cdcc573f477ecc6c91b4edf376f671
AngelVelazq <angelvelazquez.op@gmail.com>
2024-03-16 16:01:46 -0400

    Define a new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_h_
+#include <stdio.h>
+/**
+ * struct dog - define data type for variable @dog
+ * @name: input
+ * @age: input
+ * @owner: input
+ * Return: void
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
